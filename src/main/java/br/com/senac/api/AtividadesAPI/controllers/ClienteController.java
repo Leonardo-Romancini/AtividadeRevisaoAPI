d3621caa4690b4eb0679d49faa4e8bcf9ce6cbf7
LEONARDO ROMANCINI RODRIGUES VIEIRA <leonardo.vieira5@alunos.sc.senac.br>
2025-09-04 21:10:32 -0300

    arrumando o plural

--- src/main/java/br/com/senac/api/AtividadesAPI/controllers/ClienteController.java
+++ src/main/java/br/com/senac/api/AtividadesAPI/controllers/ClienteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
